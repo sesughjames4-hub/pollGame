836a4098671bae52eb296e359572c57401ea9e07
melvin <ianndiormel@gmail.com>
2025-03-11 16:51:01 +0100

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<header>
+  <h1>
+    <img src="/img/poll_ninja_logo.svg" alt="Poll Ninja Logo">
+  </h1>
+  <p>Poll Game</p>
+
+</header>
+
+<style>
+  header{
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h1{
+    margin: 0;
+    text-align: center;
+  }
+  img{
+    max-width: 180px;
+  }
+  p{
+    text-align: center;
+    font-weight: bold;
+    font-size: 30px;
+  }
+</style>
